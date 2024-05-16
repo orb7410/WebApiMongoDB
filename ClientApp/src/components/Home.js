9a12679b4f5b19bf0e5bc344cff7c510264ee1a7
orb7410 <orb7410@gmail.com>
2024-05-17 00:29:50 +0300

    student managemnt app

--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -1,31 +1,64 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
+import '../custom.css';
 
-export class Home extends Component {
-  static displayName = Home.name;
+export default function Home()
+{
+  const [students, setStudents] = useState([]);
+  useEffect(() => {
+    fetch("api/student")
+      .then(r => r.json())
+      .then(d => {
+        console.log("the students are: ", d);
+        setStudents(d);
+      })
+      .catch(e => console.log("the error fetching all students: ", e));
+  }, []);
 
-  render() {
     return (
       <main>
-        <h1>Student Manager application</h1>
+        <div className="header-container">
+          <h1 className="header">Student Manager application</h1>
+          <div className='add-btn'>
+            <a href="/new">+</a>
+          </div>
+        </div>
+
+        <div className="table-container">
+        <table>
+          <thead>
+            <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Department</th>
+            <th>Class Name</th>
+            <th>Gender</th>
+            <th>Age</th>
+            <th>Edit</th>
+            <th>Delete</th>
+            </tr>
+            </thead>
+            <tbody>
+                {students.length === 0 ? (
+                  <tr className="row waiting">
+                    <td colSpan="7">Loading...</td>
+                  </tr>
+                ) : (
+                students.map(student=> <tr key={student.id}>
+                    <td>{student.firstname}</td>
+                    <td>{student.lastname}</td>
+                    <td>{student.department}</td>
+                    <td>{student.class}</td>
+                    <td>{student.gender=== 0 ? "Female" : "Male"}</td>
+                    <td>{student.isGraduated? "Yes" : "No"}</td>
+                    <td>{student.age}</td>
+                    <td><a href={"/edit?id="+student.id}>Edit</a></td>
+                    <td>Delete</td>
+                    </tr>))
+              }
+        </tbody>
+        </table>
+        </div>
       </main>
      
     );
   }
-}
-
- // <div>
-      //   <h1>Hello, world!</h1>
-      //   <p>Welcome to your new single-page application, built with:</p>
-      //   <ul>
-      //     <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
-      //     <li><a href='https://facebook.github.io/react/'>React</a> for client-side code</li>
-      //     <li><a href='http://getbootstrap.com/'>Bootstrap</a> for layout and styling</li>
-      //   </ul>
-      //   <p>To help you get started, we have also set up:</p>
-      //   <ul>
-      //     <li><strong>Client-side navigation</strong>. For example, click <em>Counter</em> then <em>Back</em> to return here.</li>
-      //     <li><strong>Development server integration</strong>. In development mode, the development server from <code>create-react-app</code> runs in the background automatically, so your client-side resources are dynamically built on demand and the page refreshes when you modify any file.</li>
-      //     <li><strong>Efficient production builds</strong>. In production mode, development-time features are disabled, and your <code>dotnet publish</code> configuration produces minified, efficiently bundled JavaScript files.</li>
-      //   </ul>
-      //   <p>The <code>ClientApp</code> subdirectory is a standard React application based on the <code>create-react-app</code> template. If you open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code>.</p>
-      // </div>
--- ClientApp/src/components/New.js
+++ ClientApp/src/components/New.js
@@ -1,7 +1,81 @@
+import '../custom.css';
+
+const entry = {
+    id: "",
+    firstName: "",
+    lastName: "",
+    department: "",
+    className: "",
+    gender: "",
+    age: "",
+}
 export default function New (props) {
+
+    const newData = (e) => {
+        const name = e.target.name;
+        const val = e.target.value
+
+        if(name === "gender"){
+            val = Number(val)
+        }
+
+        if(name === "age"){
+            val = Number(val)
+        }
+
+        entry[name] = val
+        console.log("The New Student Is:", entry)
+  }
+  const ageOptions = [];
+        for (let age = 17; age <= 120; age++) {
+            ageOptions.push(<option value={age}>{age}</option>);
+    }
     return  (
-        <section>
+        <section className="m-20">
+            <h1>Add New Student</h1>
+
+            <div>
+                <label htmlFor="fn">First Name</label>
+                <input type="text" name="firstName" id="fn" onChange={newData}/>
+            </div>
+
+            <div>
+                <label htmlFor="ln">Last Name</label>
+                <input type="text" name="lastName" id="fn" onChange={newData}/>
+            </div>
+
+            <div>
+                <label htmlFor="dp">Department</label>
+                <input type="text" name="department" id="fn" onChange={newData}/>
+            </div>
+
+            <div>
+                <label htmlFor="cn">Class Name</label>
+                <input type="text" name="className" id="fn" onChange={newData}/>
+            </div>
+
+            <div>
+                <label htmlFor="gender">Gender</label>
+                <select name="gender" id="gender" onChange={newData}>
+                    <option value={1}>Male</option>
+                    <option value={0}>Female</option>
+                </select> 
+            </div>
+
+            <div>
+                <label htmlFor="age">Age</label>
+                <select type="text" name="age" id="fn" onChange={newData}>
+                    {ageOptions}    
+                </select>
+            </div>
             
+            <div>
+                <label htmlFor="graduated">Graduated</label>
+                <select name="isGraduates" id="graduated" onChange={newData}>
+                    <option value={1}>Yes</option>
+                    <option value={0}>No</option>
+                </select> 
+            </div>
         </section>
     )
 }
